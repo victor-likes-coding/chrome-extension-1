0b6fa723ead4c4f2f46dd395cca752eafb24a187
Victor Tran <victor.tran@tempestas.io>
2022-04-17 05:30:04 -0400

    create methods to display a single lead per add or render all leads if existing

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const myLeads = [];
+const myLeads = ["www", "http"];
 const inputEl = document.querySelector("#input-el");
+const leadsList = document.querySelector("#leads");
 
-const saveLead = (event) => {
+const saveLead = () => {
     const inputValue = inputEl.value;
     myLeads.push(inputValue);
     inputEl.value = "";
+    displayLead(inputValue);
+};
+
+const displayLead = (lead) => {
+    const li = document.createElement("li");
+    li.textContent = lead;
+    leadsList.appendChild(li);
+};
+
+const renderLeads = () => {
+    for (let lead of myLeads) {
+        const li = document.createElement("li");
+        li.textContent = lead;
+        leadsList.appendChild(li);
+    }
 };
 
 const inputBtn = document.querySelector("#input-btn");
 
 inputBtn.addEventListener("click", saveLead);
+renderLeads();
