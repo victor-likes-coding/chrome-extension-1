8d040aac24d5732c24c08b70e91462a366296f16
Victor Tran <victor.tran@tempestas.io>
2022-04-17 05:23:26 -0400

    update implementation to add lead

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const saveLead = () => {
-    console.log("Clicked");
+const myLeads = [];
+const inputEl = document.querySelector("#input-el");
+
+const saveLead = (event) => {
+    const inputValue = inputEl.value;
+    myLeads.push(inputValue);
+    inputEl.value = "";
 };
 
 const inputBtn = document.querySelector("#input-btn");
